058cbe4b049950046a313ea5bc8ae59a5eb2ecad
keishiro terashita <20153180g@gmail.com>
2022-09-07 21:12:22 +0900

    イベント編集機能できた

--- src/admin/editEvent.php
+++ src/admin/editEvent.php
@@ -9,6 +9,10 @@ if ($_SESSION['role_id'] !== '2') {
   exit();
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  require ('commitChange.php');
+}
+
 if (!isset($_GET['event_id'])){
   header('Location: events.php');
   exit();
@@ -19,7 +23,6 @@ $eventId = htmlspecialchars($_GET['event_id']);
 $stmt = $db -> prepare('SELECT * FROM events WHERE id = ?');
 $stmt -> execute([$eventId]);
 $event = $stmt -> fetch();
-var_dump($event);
 ?>
 
 <!DOCTYPE html>
@@ -45,7 +48,7 @@ var_dump($event);
   <main class="bg-gray-100 h-screen">
     <div class="w-full mx-auto py-10 px-5">
       <h2 class="text-md font-bold mb-5">イベントの編集</h2>
-      <form action="commitChange.php" method="POST">
+      <form action="" method="POST">
         <p>イベント名</p>
         <input name="name" type="text" placeholder="<?= $event['name'] ?>" value="<?= $event['name'] ?>" class="w-full p-4 text-sm mb-3">
         <p>開始日時</p>
@@ -54,6 +57,7 @@ var_dump($event);
         <input name="end_at" type="text" placeholder="<?= $event['end_at'] ?>" value="<?= $event['end_at'] ?>" class="w-full p-4 text-sm mb-3">
         <p>イベントの内容</p>
         <textarea name="detail" id="" cols="30" rows="10"><?= $event['detail'] ?></textarea>
+        <input type="hidden" name="id" value="<?= $event['id'] ?>">
         <input type="submit" value="確定" class="cursor-pointer w-full p-3 text-md text-white bg-blue-400 rounded-3xl bg-gradient-to-r from-blue-600 to-blue-300">
       </form>
     </div>
--- src/index.php
+++ src/index.php
@@ -25,7 +25,7 @@ function get_day_of_week($w)
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+c
   <title>Schedule | POSSE</title>
 </head>
 
