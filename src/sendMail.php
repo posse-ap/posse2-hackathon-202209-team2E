bff699ac329145df65f7b36d0bd9477a17dd335a
keishiro terashita <20153180g@gmail.com>
2022-09-07 12:37:08 +0900

    ターミナル上でコマンドを実行したとき、翌日にイベントがあればユーザー全員にメールを送信

--- src/sendMail.php
+++ src/sendMail.php
@@ -8,42 +8,34 @@ $eventDate = [
   'start' => date('Y/m/d', strtotime('+1 day')) . ' 00:00',
   'end' => date('Y/m/d', strtotime('+1 day')) . ' 23:59'
 ];
-$stmt = $db -> prepare('SELECT * FROM events where start_at > ? AND start_at < ?');
+$stmt = $db -> prepare(
+  'SELECT id, name, 
+  TIME_FORMAT(start_at, "%H:%i") start_at, 
+  TIME_FORMAT(end_at, "%H:%i") end_at 
+  FROM events where start_at > ? AND start_at < ?');
 $stmt -> execute([$eventDate['start'], $eventDate['end']]);
 $events = $stmt -> fetchAll();
-var_dump($events);
 
 mb_language('ja');
 mb_internal_encoding('UTF-8');
+foreach($events as $event){
+  foreach($users as $user){
+    $to = $user['email'];
+    $subject = 'イベントの前日リマインド';
+    $name = $user['name'];
+    $eventName = $event['name']; 
+    $startAt = $event['start_at'];
+    $endAt = $event['end_at'];
+    $headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
+    $body = <<<EOT
+    {$name}さん
 
-foreach($users as $user){
-  $to = $user['email'];
-  $subject = 'イベントの前日リマインド';
-  $name = $user['name'];
-  $event = 
-  $body = <<<EOT
-  {$name}さん
-
-
-  EOT;
+    明日、{$eventName}を
+    {$startAt} ~ {$endAt}
+    に開催します。お楽しみに！
+    EOT;
+    mb_send_mail($to, $subject, $body, $headers);
+  }
 }
 
-$to = "hackathon-teamX@posse-ap.com";
-$subject = "PHPからメール送信サンプル";
-$body = "本文";
-$headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
-
-$name = "テスト";
-$date = "2021年08月01日（日） 21:00~23:00";
-$event = "縦モク";
-$body = <<<EOT
-{$name}さん
-
-${date}に${event}を開催します。
-参加／不参加の回答をお願いします。
-
-http://localhost/
-EOT;
-
-// mb_send_mail($to, $subject, $body, $headers);
 echo "メールを送信しました";
