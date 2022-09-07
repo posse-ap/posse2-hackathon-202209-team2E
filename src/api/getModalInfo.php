85d0880eb334583835ac2c2d317ea398e232ff30
keishiro terashita <20153180g@gmail.com>
2022-09-07 17:06:26 +0900

    イベントの内容が登録されていればモーダル内で確認できる

--- src/api/getModalInfo.php
+++ src/api/getModalInfo.php
@@ -5,14 +5,18 @@ header('Content-Type: application/json; charset=UTF-8');
 if (isset($_GET['eventId'])) {
   $eventId = htmlspecialchars($_GET['eventId']);
   try {
-    $stmt = $db->prepare('SELECT events.id, events.name, events.start_at, events.end_at, count(event_attendance.id) AS total_participants FROM events LEFT JOIN event_attendance ON events.id = event_attendance.event_id WHERE events.id = ? GROUP BY events.id');
+    $stmt = $db->prepare('SELECT events.id, events.name, events.start_at, events.end_at, events.detail, count(event_attendance.id) AS total_participants FROM events LEFT JOIN event_attendance ON events.id = event_attendance.event_id WHERE events.id = ? GROUP BY events.id');
     $stmt->execute(array($eventId));
     $event = $stmt->fetch();
     
     $start_date = strtotime($event['start_at']);
     $end_date = strtotime($event['end_at']);
 
-    $eventMessage = date("Y年m月d日", $start_date) . '（' . get_day_of_week(date("w", $start_date)) . '） ' . date("H:i", $start_date) . '~' . date("H:i", $end_date) . 'に' . $event['name'] . 'を開催します。<br>ぜひ参加してください。';
+    if($event['detail']){
+      $eventMessage = nl2br($event['detail']);
+    } else {
+      $eventMessage = date("Y年m月d日", $start_date) . '（' . get_day_of_week(date("w", $start_date)) . '） ' . date("H:i", $start_date) . '~' . date("H:i", $end_date) . 'に' . $event['name'] . 'を開催します。<br>ぜひ参加してください。';
+    }
 
     if ($event['id'] % 3 === 1) $status = 0;
     elseif ($event['id'] % 3 === 2) $status = 1;
