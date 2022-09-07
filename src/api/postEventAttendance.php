ce027d1fadab0c4bc00c7260004ebfdbe816c06b
Akira <38104713+akira1515@users.noreply.github.com>
2022-09-07 14:10:27 +0900

    命名を変更
    Co-authored-by: Jin Aoyagi <dousummer3@gmail.com>

--- src/api/postEventAttendance.php
+++ src/api/postEventAttendance.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json; charset=UTF-8');
 session_start();
 
 $eventId = $_POST['eventId'];
-$USER_ID = $_SESSION['user_id'];
+$userId = $_SESSION['user_id'];
 $status = $_POST['status'];
 
 $stmt = $db->prepare("INSERT INTO event_attendance(event_id, user_id, status) VALUES (?,?,?)");
