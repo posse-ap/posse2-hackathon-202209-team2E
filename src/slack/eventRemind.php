4bda1088188b997c7e3ce4dd32feb5790f4b07a6
andmatcha <jj.mcr185@gmail.com>
2022-09-08 22:23:06 +0900

    dbからslack_idを取るようにした

--- src/slack/eventRemind.php
+++ src/slack/eventRemind.php
@@ -16,7 +16,8 @@ $stmt = $db->prepare(
   events.detail,
   TIME_FORMAT(start_at, "%H:%i") start_at,
   TIME_FORMAT(end_at, "%H:%i") end_at ,
-  users.name name
+  users.name name,
+  users.slack_id slack_id
   FROM
   events RIGHT JOIN event_attendance ON events.id = event_id LEFT JOIN users ON user_id = users.id
   where start_at > ? AND start_at < ? AND status = "presence"'
@@ -29,6 +30,7 @@ $reminders = [];
 
 foreach ($results as $result) {
   $name = $result['name']; //参加者名
+  $slack_id = $result['slack_id']; //slack_id
   $eventName = $result['event']; //イベント名
   $detail = $result['detail']; // イベント内容
   $startAt = $result['start_at']; // 開始
@@ -44,16 +46,18 @@ foreach ($results as $result) {
     $reminders += [
       $eventName => [
         'text' => $body,
-        'members' => []
+        'slack_ids' => []
       ]
     ];
   }
   // メンバーを一人ずつ追加
-  array_push($reminders[$eventName]['members'], $name);
+  if (!is_null($slack_id)) {
+    array_push($reminders[$eventName]['slack_ids'], $slack_id);
+  }
 }
 
 foreach ($reminders as $reminder) {
-  sendMessage($reminder['text'], $reminder['members'], $token['SLACK_TOKEN']);
+  sendMessage($reminder['text'], $reminder['slack_ids'], $token['SLACK_TOKEN']);
 }
 
 echo '送信完了\n';
--- src/slack/sendMessage.php
+++ src/slack/sendMessage.php
@@ -1,19 +1,13 @@
 <?php
 
 // 第一引数に本文、第二引数にメンションする人
-function sendMessage(string $text, array $members, $token)
+function sendMessage(string $text, array $slack_ids, $token)
 {
-  $memberId = [
-    '寺下渓志郎' => '<@U041VQ2JKFT>',
-    '青柳仁' => '<@U041AMSQF1C>',
-    '寺嶋里紗' => '<@U041W18Q6HX>',
-    '小林哲' => '<@U041HGE5343>'
-  ];
   $mention = '';
 
   // メンション作成
-  foreach ($members as $member) {
-    $mention = $mention . ' ' . $memberId[$member];
+  foreach ($slack_ids as $slack_id) {
+    $mention = $mention . ' <@' . $slack_id . '>';
   }
 
   // 本文とメンションを結合
