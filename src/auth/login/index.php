2846746307bf3c7f795313c959c0c79418de469a
andmatcha <jj.mcr185@gmail.com>
2022-09-08 21:36:35 +0900

    タイトルを修正

--- src/auth/login/index.php
+++ src/auth/login/index.php
@@ -53,7 +53,7 @@ unset($_SESSION['error_message']);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-  <title>Schedule | POSSE</title>
+  <title>ログイン | POSSE</title>
 </head>
 
 <body>
--- src/index.php
+++ src/index.php
@@ -84,7 +84,7 @@ function get_day_of_week($w)
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-  <title>Schedule | POSSE</title>
+  <title>イベント一覧 | POSSE</title>
 </head>
 
 <body>
